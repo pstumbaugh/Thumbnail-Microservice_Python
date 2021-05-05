9cb402c86bf48b0b0ff5d37a03eca24d730f5f93
Pat <stumbaup@oregonstate.edu>
2021-05-05 16:47:38 -0700

    localhost working

--- receive.py
+++ receive.py
@@ -1,21 +1,32 @@
 #!/usr/bin/env python
 import pika
-
-connection = pika.BlockingConnection(pika.ConnectionParameters(
-    host='localhost'))
-channel = connection.channel()
-
-channel.queue_declare(queue='hello')
-
-print(' [*] Waiting for messages. To exit press CTRL+C')
+import sys
+import os
 
 
-def callback(ch, method, properties, body):
-    print(" [x] Received %r" % (body,))
+def main():
+    connection = pika.BlockingConnection(
+        pika.ConnectionParameters(host='localhost'))
+    channel = connection.channel()
+
+    channel.queue_declare(queue='hello')
+
+    def callback(ch, method, properties, body):
+        print(" [x] Received %r" % body)
+
+    channel.basic_consume(
+        queue='hello', on_message_callback=callback, auto_ack=True)
+
+    print(' [*] Waiting for messages. To exit press CTRL+C')
+    channel.start_consuming()
 
 
-channel.basic_consume(callback,
-                      queue='hello',
-                      no_ack=True)
-
-channel.start_consuming()
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        print('Interrupted')
+        try:
+            sys.exit(0)
+        except SystemExit:
+            os._exit(0)
