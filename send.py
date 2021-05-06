11c3e2e01f82fb0ba3acebba7afdb2e447a53808
Pat <stumbaup@oregonstate.edu>
2021-05-05 17:53:32 -0700

    update

--- send.py
+++ send.py
@@ -7,6 +7,6 @@ channel = connection.channel()
 
 channel.queue_declare(queue='hello')
 
-channel.basic_publish(exchange='', routing_key='hello', body='Pat!')
+channel.basic_publish(exchange='', routing_key='hello', body='Hello World!')
 print(" [x] Sent 'Hello World!'")
 connection.close()
